ff63c6ded14ba52dbc0301879b90717c064fb7da
bsscco <bsscco@gmail.com>
2020-02-12 08:25:07 +0900

    utils를 서브모듈로 변경

--- utils/OkHttpLoggingInterceptor.kt
+++ utils/OkHttpLoggingInterceptor.kt
@@ -3,7 +3,7 @@ package base.utils
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class LoggingInterceptor : Interceptor {
+class OkHttpLoggingInterceptor : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
